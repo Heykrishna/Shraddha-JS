c3527a9dfd5dfeaaa9e9d72e06ec7695d32cb430
Heykrishna <heykrishna35@gmail.com>
2025-08-19 19:27:17 +0530

    Pushing loops and strings  code

--- loopsAndStrings.js/basic.js
+++ loopsAndStrings.js/basic.js
@@ -1,3 +1,18 @@
 for(let count = 1; count <= 5; count++){
     console.log("Apna college");
 }
+
+console.log("Loop has ended");
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+let sum = 0;
+
+for(let i = 1; i <=5; i++){
+    sum = sum + i;
+}
+
+console.log(sum);
+
+
